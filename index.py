3d189e060521789cdaf5ff7514ecfaac2d10656a
Alexander Polanco <alexanderrpolanco11@gmail.com>
2022-08-18 12:00:39 -0400

    agregue la conexion a mongoDb

--- index.py
+++ index.py
@@ -1,1 +1,18 @@
-print("Estamos trabajando en el proyecto final de electiva....")
+from dataclasses import dataclass
+from http import client
+from pydoc import cli
+from pymongo import MongoClient
+
+
+client = MongoClient(
+    "mongodb+srv://electiva:electiva@cluster0.lyulc0c.mongodb.net/?retryWrites=true&w=majority")
+db = client.get_database("test")
+records = db.nom
+# new_s = {
+#     "name": "Alexander",
+#     "Lastname": "Polanco Moreno"
+# }
+# records.insert_one(new_s)
+datos = list(records.find())
+for i in range(len(datos)):
+    print(datos[i]["name"])
